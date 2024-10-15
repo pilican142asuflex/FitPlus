10c9c92f48089c4072059a19d0977c7be9dbb34b
pilican142asuflex <133699379+pilican142asuflex@users.noreply.github.com>
2024-10-15 19:55:50 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC8xaXSPUhLr8D8i_6V3epYPUhsj8bVz0c",
+    apiKey: API_KEY,
     authDomain: "fitplus-edc14.firebaseapp.com",
-    projectId: "fitplus-edc14",
+    projectId: PROJECT_ID,
     storageBucket: "fitplus-edc14.appspot.com",
-    messagingSenderId: "556369234732",
-    appId: "1:556369234732:web:2a07b981bbeacec3227a89",
-    measurementId: "G-W43533L41D"
+    messagingSenderId: MSG_ID,
+    appId: APP_ID,
+    measurementId: MES_ID
   };
 
 const app = initializeApp(firebaseConfig);
